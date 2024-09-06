cbdebc1c303e0b3b1ebe33754761fe3bdafe33c3
Matti Salokangas <matti.salokangas@gmail.com>
2024-09-06 18:23:16 +0200

    add exercise

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+		]
+	}
+};
 
 export default nextConfig;
